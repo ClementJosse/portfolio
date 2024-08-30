ffb2c704867de1cea3f438f18638cbc04ed2c383
ClementJosse <clement.b.josse@gmail.com>
2024-08-30 14:21:43 +0200

    ajout de la font et des logo svg des techno pour le background

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+// Ajoutez ce lien pour importer la police Montserrat depuis Google Fonts
+const link = document.createElement('link');
+link.href = 'https://fonts.googleapis.com/css2?family=Montserrat:wght@200;300;400;500;600;700;800;900&display=swap';
+link.rel = 'stylesheet';
+document.head.appendChild(link);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
